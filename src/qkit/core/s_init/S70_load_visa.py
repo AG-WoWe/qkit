ce3db7ec273897111cef80b46e6b8b0a85d74664
So-pi <sophie.klingel@kit.edu>
2026-01-22 17:44:50 +0100

    Update S70_load_visa.py
    Quick fix pyvisa

--- src/qkit/core/s_init/S70_load_visa.py
+++ src/qkit/core/s_init/S70_load_visa.py
@@ -17,9 +17,9 @@ def _load_visa():
         raise type(e)('Failed loading visa. Check if you have NI VISA or pyvisa-py installed. Original error: ' + str(e))
     else:
         from pkg_resources import get_distribution
-        # from distutils.version import LooseVersion
-        # if LooseVersion(get_distribution('pyvisa').version) < LooseVersion("1.5.0"):
-        if parse(version('pyvisa')) < parse("1.5.0"):
+        from distutils.version import LooseVersion
+        if LooseVersion(get_distribution('pyvisa').version) < LooseVersion("1.5.0"):
+        # if parse(version('pyvisa')) < parse("1.5.0"):
             logging.warning("Old pyvisa version loaded. Please update to a version > 1.5.x")
             # logging.warning("Old pyvisa version loaded. Please update to a version > 1.5.x")
             # compatibility with old visa lib
